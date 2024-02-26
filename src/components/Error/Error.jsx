e10bcc1bae2a84f8ed15197721c94c4a7f5772af
requ19 <azizmailcom1910@gmail.com>
2024-02-26 15:26:59 +0600

    correction2.0

--- src/components/Error/Error.jsx
+++ src/components/Error/Error.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import cry from './cry.png'
+import './Error.css'
 const Error = ({weather}) => {
   return (
-    <div>
-        {weather.cod === '404' ? <div> <img src={cry} alt="img" width="290px" />  Нет такого города </div>  : <div> <img src={cry} alt="img" width="290px" />  Введите название города</div>
+    <div className='error'>
+        {weather.cod === '404' ? <div> <img src={cry} alt="img" width="290px" />  <p>Нет такого города</p> </div>  : <div> <img src={cry} alt="img" width="290px" /> <p> Введите название города</p></div>
         }
 
         
